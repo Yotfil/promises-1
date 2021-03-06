885e7e57063c45f7aa767de666a7e8e1971643de
Jefry Guevara <jefryeguevarav@gmail.com>
2021-03-06 13:43:28 -0500

    feat(f): f
    df

--- src/DOM.js
+++ src/DOM.js
@@ -15,7 +15,7 @@ console.log(hola);
 
 // Side effect
 window.addEventListener('DOMContentLoaded', function () {
-    console.log('el DOM esta listo');
+    console.log('el DOM esta listo!!!');
 
     for (const llave of Object.keys(elementosDOM)) {
         elementosDOM[llave] = document.getElementById(llave);
